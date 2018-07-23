0a27a76fb6d3a1d77ded16da1130f2b847c69a93
Sakura Rapolu <rapolusakura@gmail.com>
2018-07-23 15:54:03 -0700

    better version of the current location?

--- WakeMe/WakeMe/ViewController.swift
+++ WakeMe/WakeMe/ViewController.swift
@@ -13,24 +13,36 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
-    private var locationManager: CLLocationManager!
+    private var locationManager = CLLocationManager()
     private var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
-        
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
+        mapView.showsUserLocation = true
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
         
-        // Check for Location Services
-        
-        if CLLocationManager.locationServicesEnabled() {
+        //Check for Location Services
+        if (CLLocationManager.locationServicesEnabled()) {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
+        }
+        //Zoom to user location
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 200, 200)
+        mapView.setRegion(viewRegion, animated: false)
+        
+        DispatchQueue.main.async {
+            self.locationManager.startUpdatingLocation()
         }
     }
     
